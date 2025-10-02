b091c0b59b5ba166ae29945c9ac4c89ec8db4a08
Ye Htet Rohim <80707945+yehtet2006@users.noreply.github.com>
2025-10-02 13:22:06 +0200

    UC13: Deels klaar, nog de dropdown menu fixen zodat het de namen van de producten tonen.

--- Grocery.App/ViewModels/GroceryListViewModel.cs
+++ Grocery.App/ViewModels/GroceryListViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
 using System.Collections.ObjectModel;
+using Grocery.Core;
 
 namespace Grocery.App.ViewModels
 {
@@ -10,11 +11,15 @@ namespace Grocery.App.ViewModels
     {
         public ObservableCollection<GroceryList> GroceryLists { get; set; }
         private readonly IGroceryListService _groceryListService;
+        private readonly IClientService _clientService;
 
-        public GroceryListViewModel(IGroceryListService groceryListService) 
+        public Client? Client { get; set; } // <- deze property is nodig voor binding
+        public GroceryListViewModel(IGroceryListService groceryListService, IClientService clientService) 
         {
             Title = "Boodschappenlijst";
             _groceryListService = groceryListService;
+            _clientService = clientService;
+            Client = clientService.Get(3);
             GroceryLists = new(_groceryListService.GetAll());
         }
 
@@ -24,6 +29,21 @@ namespace Grocery.App.ViewModels
             Dictionary<string, object> paramater = new() { { nameof(GroceryList), groceryList } };
             await Shell.Current.GoToAsync($"{nameof(Views.GroceryListItemsView)}?Titel={groceryList.Name}", true, paramater);
         }
+
+        [RelayCommand]
+        public async Task ShowBoughtProducts()
+        {
+            var currentClient = _clientService.Get(3);
+
+            if (currentClient == null)
+                return;
+
+            if (currentClient.Role == Role.Admin)
+            { 
+                await Shell.Current.GoToAsync(nameof(Views.BoughtProductsView));
+            }
+        }
+        
         public override void OnAppearing()
         {
             base.OnAppearing();
--- Grocery.Core.Data/Repositories/ClientRepository.cs
+++ Grocery.Core.Data/Repositories/ClientRepository.cs
@@ -11,11 +11,13 @@ namespace Grocery.Core.Data.Repositories
         public ClientRepository()
         {
             clientList = [
-                new Client(1, "M.J. Curie", "user1@mail.com", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08="),
-                new Client(2, "H.H. Hermans", "user2@mail.com", "dOk+X+wt+MA9uIniRGKDFg==.QLvy72hdG8nWj1FyL75KoKeu4DUgu5B/HAHqTD2UFLU="),
-                new Client(3, "A.J. Kwak", "user3@mail.com", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=")
+                new Client(1, "M.J. Curie", "user1@mail.com", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08=", Role.None),
+                new Client(2, "H.H. Hermans", "user2@mail.com", "dOk+X+wt+MA9uIniRGKDFg==.QLvy72hdG8nWj1FyL75KoKeu4DUgu5B/HAHqTD2UFLU=",Role.None),
+                new Client(3, "A.J. Kwak", "user3@mail.com", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=", Role.Admin)
             ];
         }
+        
+        // In Client Repo koppel je de rol Role.Admin aan user3 (= admin).
 
         public Client? Get(string email)
         {
@@ -34,4 +36,5 @@ namespace Grocery.Core.Data.Repositories
             return clientList;
         }
     }
+    
 }
